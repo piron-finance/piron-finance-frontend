0523307f20c13a5426816dfca6110951c19ad348
Ernest Nnamdi <Bulaveur@ErnestMackbookAir.local>
2024-01-28 03:33:07 +0100

    chore: added new sections and responsiveness

--- app/(marketing)/_components/Heading.tsx
+++ app/(marketing)/_components/Heading.tsx
@@ -8,12 +8,17 @@ const font = DM_Sans({ weight: "700", subsets: ["latin"] });
 
 const Heading = () => {
   return (
-    <div className="flex flex-col justify-center items-center gap-4  px-32 py-8 text-[#323536]">
+    <div className="flex flex-col justify-center items-center gap-4  px-8 lg:px-32 py-20 text-[#323536]">
       <div className="flex flex-col items-center gap-8 justify-center max-w-5xl ">
-        <h1 className={cn("text-center  text-6xl  ", font.className)}>
+        <h1
+          className={cn(
+            "text-center text-3xl md:text-4xl lg:text-6xl  ",
+            font.className
+          )}
+        >
           Decentralized Asset Management for the Next-gen Investor
         </h1>
-        <p className="text-center max-w-2xl text-muted-foreground ">
+        <p className="text-center text-sm md:text-base max-w-2xl text-muted-foreground ">
           Piron brings asset originators and investors together, while ensuring
           a transparent, secured, and enhanced liquidity for financial and
           real-world assets.
