7595e53470984ef8e32c1df3358dbbc6014eaf60
Ernest Nnamdi <ernestnnamdi@Ernests-MacBook-Pro-2.local>
2024-03-22 03:50:30 +0100

    feat: header done

--- app/(dashboard)/(routes)/pools/[details]/_components/header.tsx
+++ app/(dashboard)/(routes)/pools/[details]/_components/header.tsx
@@ -1,32 +1,31 @@
 import React from "react";
-import Image from "next/image";
 
 const Header = () => {
   return (
     <div className="flex flex-col gap-6 bg-white mx-2 rounded-2xl shadow-sm  ">
-      <div className=" ml-10 mt-6">
+      <div className="ml-10 mt-6">
         <h1 className="text-lg font-medium">Overview</h1>
       </div>
 
-      <div className="flex items-center justify-around  my-12 ">
-        <div className="flex flex-col items-center gap-4">
-          <h1 className="text-2xl font-medium text-[#32393A]">5.32%</h1>
-          <h1 className="text-4xl font-medium text-[#414B4C]">Est. Yield</h1>
+      <div className="flex justify-evenly gap-5 mx-6 my-12 ">
+        <div className="flex flex-col items-center justify-center gap-4 bg-[#FFF9F2] py-10 px-12 flex-grow">
+          <h1 className="text-xl font-medium text-[#32393A]">5.32%</h1>
+          <h1 className="text-sm font-medium text-[#414B4C]">Est. Yield</h1>
         </div>
 
-        <div className="flex flex-col items-center gap-4">
-          <h1 className="text-2xl font-medium text-[#32393A]">$8,649,560.23</h1>
-          <h1 className="text-4xl font-medium text-[#414B4C]">Target Raise</h1>
+        <div className="flex flex-col items-center gap-4 bg-[#FFF2FA] py-10 px-12 flex-grow">
+          <h1 className="text-xl font-medium text-[#32393A]">$8,649,560.23</h1>
+          <h1 className="text-sm font-medium text-[#414B4C]">Target Raise</h1>
         </div>
 
-        <div className="flex flex-col items-center gap-4">
-          <h1 className="text-2xl font-medium text-[#32393A]">5,649,560.23</h1>
-          <h1 className="text-4xl font-medium text-[#414B4C]">Current Raise</h1>
+        <div className="flex flex-col items-center gap-4 bg-[#F2FFF4] py-10 px-12 flex-grow">
+          <h1 className="text-xl font-medium text-[#32393A]">5,649,560.23</h1>
+          <h1 className="text-sm font-medium text-[#414B4C]">Current Raise</h1>
         </div>
 
-        <div className="flex flex-col items-center gap-4">
-          <h1 className="text-2xl font-medium text-[#32393A]">Real Estate</h1>
-          <h1 className="text-4xl font-medium text-[#414B4C]">Asset Type</h1>
+        <div className="flex flex-col items-center gap-4 bg-[#F2F4FF] py-10 px-12 flex-grow">
+          <h1 className="text-xl font-medium text-[#32393A]">Real Estate</h1>
+          <h1 className="text-sm font-medium text-[#414B4C]">Asset Type</h1>
         </div>
       </div>
     </div>
--- app/(dashboard)/(routes)/pools/[details]/page.tsx
+++ app/(dashboard)/(routes)/pools/[details]/page.tsx
@@ -1,10 +1,12 @@
 import Navbar from "@/app/(dashboard)/_components/Navbar";
 import React from "react";
+import Header from "./_components/header";
 
 const page = () => {
   return (
     <div className="flex flex-col h-full ">
       <Navbar status="Open" page="Piron Credit Deal 1" dynamic />
+      <Header />
     </div>
   );
 };
