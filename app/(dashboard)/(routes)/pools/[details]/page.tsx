cdf2f86cafe5f7c634eee2c9400e4b596cc0eb92
Ernest Nnamdi <ernestnnamdi@Ernests-MacBook-Pro-2.local>
2024-03-23 23:14:24 +0100

    feat: added originator section to details page

--- app/(dashboard)/(routes)/pools/[details]/page.tsx
+++ app/(dashboard)/(routes)/pools/[details]/page.tsx
@@ -1,7 +1,8 @@
 import Navbar from "@/app/(dashboard)/_components/Navbar";
 import Header from "./_components/header";
-import { detailsData } from "./_components/dummy";
+import { TeamData, detailsData } from "./_components/dummy";
 import { Button } from "@/components/ui/button";
+import TeamCard from "./_components/TeamCard";
 
 const page = () => {
   return (
@@ -108,6 +109,80 @@ const page = () => {
           <div className="flex flex-col gap-3 bg-white rounded-xl shadow-sm p-8">
             <h1 className="text-[#546162] font-medium">Funding Strategy</h1>
             <p className="text-muted-foreground text-sm">Immediate Funding</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="bg-white shadow-sm mx-2 rounded-xl p-8">
+        <h1 className="text-lg text-muted-foreground font-medium mb-8">
+          Asset Originator
+        </h1>
+        <div className="flex ">
+          <div className="flex flex-col gap-4 w-1/2">
+            <div className="flex flex-col gap-3 ">
+              <h1 className="text-[#546162] text-2xl font-semibold ">
+                Description
+              </h1>
+              <p className="text-muted-foreground text-sm">
+                Adapt3r Digital LLC (“Adapt3r Digital”) is a U.S.-based fund
+                manager specializing in short-term U.S. Treasury Bills. Our team
+                combines multifaceted expertise in the asset management
+                industry, spanning private credit, securitization, and digital
+                assets.
+              </p>
+            </div>
+
+            <div className="flex flex-col gap-3 ">
+              <h1 className="text-xl font-medium text-[#546162]">Numbers</h1>
+              <div className="flex items-center gap-12">
+                <div className="flex flex-col gap-2">
+                  <h1 className="text-sm text-muted-foreground">
+                    Total raised{" "}
+                    <span className="font-semibold text-gray-600">
+                      $17,100,000
+                    </span>
+                  </h1>
+                  <h1 className="text-sm text-muted-foreground">
+                    View on-chain reputation <span className="font-semibold text-gray-600">0xdc01...5d476a</span>
+                  </h1>
+                </div>
+                <div>
+                <h1 className="text-sm text-muted-foreground">
+                    Deals Launched <span className="font-semibold text-gray-600">>2</span>
+                  </h1>
+                  <h1 className="text-sm text-muted-foreground">
+                    Health Status <span className="font-semibold text-gray-600">good</span>
+                  </h1>
+                </div>
+              </div>
+            </div>
+
+            <div className="flex flex-col gap-2">
+            <h1 className="text-xl font-medium text-[#546162]">Contact</h1>
+              <div className="text-muted-foreground text-sm">
+                <p>Email: ir@adapt3r.com</p>
+                <p>Website: adapt3r.com</p>
+              </div>
+            </div>
+          </div>
+
+          <div className="flex flex-col gap-4 w-1/2">
+          <h1 className="text-[#546162] text-2xl font-semibold ">Team</h1>
+          <p className="text-muted-foreground text-sm">
+              Our team combines multifaceted expertise in the asset management
+              industry, spanning private credit, securitization, and digital
+              assets.
+            </p>
+            <div className="grid grid-cols-3 gap-3 max-w-lg">
+              {TeamData.map((item, index) => (
+                <TeamCard
+                  key={index}
+                  img={item.img}
+                  role={item.role}
+                  name={item.name}
+                />
+              ))}
+            </div>
           </div>
         </div>
       </div>
